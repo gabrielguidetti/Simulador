ad5bac706814e0547e62de56bc6166e3b0cc2fd6
Gabriel Guidetti <gabrielguidetti@gmail.com>
2025-10-15 22:23:52 -0300

    Remove comments

--- Simulador/Models/MT.cs
+++ Simulador/Models/MT.cs
@@ -7,77 +7,19 @@ namespace Simulador.Models
 {
     public class MT : IRNGenerator
     {
-        #region constants
-        /// <summary>
-        /// N
-        /// </summary>
         private static readonly int N = 624;
-
-        /// <summary>
-        /// M
-        /// </summary>
         private static readonly int M = 397;
-
-        /// <summary>
-        /// Constant vector a
-        /// </summary>
         private readonly UInt32 MATRIX_A = 0x9908b0df;
-
-        /// <summary>
-        /// most significant w-r bits
-        /// </summary>
         private readonly UInt32 UPPER_MASK = 0x80000000;
-
-        /// <summary>
-        /// least significant r bits
-        /// </summary>
         private readonly UInt32 LOWER_MASK = 0x7fffffff;
-
-        /// <summary>
-        /// Tempering mask B
-        /// </summary>
         private readonly UInt32 TEMPERING_MASK_B = 0x9d2c5680;
-
-        /// <summary>
-        /// Tempering mask C
-        /// </summary>
         private readonly UInt32 TEMPERING_MASK_C = 0xefc60000;
-
-        /// <summary>
-        /// Last constant used for generation
-        /// </summary>
         private readonly double FINAL_CONSTANT = 2.3283064365386963e-10;
-        #endregion
-
-        #region singleton
-        private static volatile MT instance;
-
-        private static object syncRoot = new Object();
-
-        public static MT Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    lock (syncRoot)
-                    {
-                        if (instance == null)
-                        {
-                            instance = new MT();
-                        }
-                    }
-                }
-                return instance;
-            }
-        }
 
         public MT()
         {
         }
-        #endregion
 
-        #region helpers methods
         private ulong TEMPERING_SHIFT_U(ulong y)
         {
             return y >> 11;
@@ -97,28 +39,10 @@ namespace Simulador.Models
         {
             return y >> 18;
         }
-        #endregion
 
-        #region properties
-
-        /// <summary>
-        /// the array for the state vector
-        /// </summary>
         private readonly ulong[] mt = new ulong[625];
-
-        /// <summary>
-        /// mti==N+1 means mt[N] is not initialized 
-        /// </summary>
         private int mti = N + 1;
-        #endregion
 
-        #region engine
-        /// <summary>
-        /// setting initial seeds to mt[N] using
-        /// the generator Line 25 of Table 1 in
-        /// [KNUTH 1981, The Art of Computer Programming Vol. 2 (2nd Ed.), pp102] 
-        /// </summary>
-        /// <param name="seed"></param>
         private void sgenrand(ulong seed)
         {
             mt[0] = seed & 0xffffffff;
@@ -130,14 +54,13 @@ namespace Simulador.Models
         {
             ulong y;
             ulong[] mag01 = new ulong[2] { 0x0, MATRIX_A };
-            /* mag01[x] = x * MATRIX_A  for x=0,1 */
 
             if (mti >= N)
-            { /* generate N words at one time */
+            { 
                 int kk;
 
-                if (mti == N + 1)   /* if sgenrand() has not been called, */
-                    sgenrand(4357); /* a default initial seed is used   */
+                if (mti == N + 1)
+                    sgenrand(4357);
 
                 for (kk = 0; kk < N - M; kk++)
                 {
@@ -161,28 +84,14 @@ namespace Simulador.Models
             y ^= TEMPERING_SHIFT_T(y) & TEMPERING_MASK_C;
             y ^= TEMPERING_SHIFT_L(y);
 
-            //reals: (0,1)-interval
-            //return y; for integer generation
             return ((double)y * FINAL_CONSTANT);
         }
-        #endregion
 
-        #region public methods
-        /// <summary>
-        /// Generate a random number between 0 and 1
-        /// </summary>
-        /// <returns></returns>
         public double Generate()
         {
             return this.genrand();
         }
 
-        /// <summary>
-        /// Generate an int between two bounds
-        /// </summary>
-        /// <param name="lowerBound">The lower bound (inclusive)</param>
-        /// <param name="higherBound">The higher bound (inclusive)</param>
-        /// <returns></returns>
         public double Generate(int lowerBound, int higherBound)
         {
             if (higherBound < lowerBound)
@@ -192,12 +101,6 @@ namespace Simulador.Models
             return Convert.ToInt32(Math.Floor(this.Generate(lowerBound * 1.0d, higherBound * 1.0d)));
         }
 
-        /// <summary>
-        /// Generate a double between two bounds
-        /// </summary>
-        /// <param name="lowerBound">The lower bound (inclusive)</param>
-        /// <param name="higherBound">The higher bound (inclusive)</param>
-        /// <returns>The random num or NaN if higherbound is lower than lowerbound</returns>
         public double Generate(double lowerBound, double higherBound)
         {
             if (higherBound < lowerBound)
@@ -225,6 +128,5 @@ namespace Simulador.Models
 
             return result;
         }
-        #endregion
     }
 }
--- Simulador/Models/MyFontResolver.cs
+++ Simulador/Models/MyFontResolver.cs
@@ -7,33 +7,10 @@ namespace Simulador.Models
 {
     public class MyFontResolver : IFontResolver
     {
-        /*public FontResolverInfo ResolveTypeface(string familyName, bool isBold, bool isItalic)
-        {
-            if (familyName == "Arial")
-            {
-                if (isBold && isItalic) return new FontResolverInfo("Arial#BoldItalic");
-                if (isBold) return new FontResolverInfo("Arial#Bold");
-                if (isItalic) return new FontResolverInfo("Arial#Italic");
-                return new FontResolverInfo("Arial#Regular");
-            }
-            return null;
-        }
-
-        public byte[] GetFont(string faceName)
-        {
-            switch (faceName)
-            {
-                case "Arial#Regular":
-                    return File.ReadAllBytes("C:\\Users\\User\\Desktop\\arial.ttf");
-            }
-            return null;
-        }*/
-
         public byte[] GetFont(string faceName)
         {
             var assembly = Assembly.GetExecutingAssembly();
 
-            // nome do recurso: [namespace do projeto].[pasta].[arquivo]
             string resourceName = "Simulador.Src.arial.ttf";
 
             using (Stream stream = assembly.GetManifestResourceStream(resourceName))
